313a6b545ae49b66cf62ff71b2fbe4f815b4dae5
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-04-18 12:57:56 +0530

    Time: 2 ms (47.31%) | Memory: 7.1 MB (64.95%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,9 +4,8 @@ public:
         int steps[n+1];
         steps[0]=1;
         steps[1]=1;
-        int i=2;
-        while(i<=n){
-                steps[i]=steps[i-1]+steps[i-2];i++;
+        for(int i=2;i<=n;i++){
+                steps[i]=steps[i-1]+steps[i-2];
             }
         return steps[n];
     }
