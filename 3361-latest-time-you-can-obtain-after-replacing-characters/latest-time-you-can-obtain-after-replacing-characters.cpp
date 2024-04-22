845a20636a5614517cbc5cdb170f3bfc4fb52091
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-04-22 14:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (67.51%) - LeetSync

--- 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.cpp
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     string findLatestTime(string s) {
-        vector<int> ind;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='?'){
-                ind.push_back(i);
-            }
-        }
-        for(int i=0;i<ind.size();i++){
-            switch(ind[i])
-            {
-            case 0:
+            if(s[0]=='?'){
                 if(s[1]!='1' && s[1]!='?' && s[1]!='0'){
                     s[0]='0';
                 }else{
                     s[0]='1';
                 }
-                break;
-            case 1:
+                }// true from here ..
+            if(s[1]=='?'){
                 if(s[0]!='1' ){
                     s[1]='9';
                 }else{
                     s[1]='1';
                 }
-                break;
-            // true from here ..
-            case 3:
-                s[3]='5';
-                break;
-            case 4:
-                s[4]='9';
-                break;
-            default:
-                cout<<"invalid ! ";
-        }
+            }
+            
+            if( s[3]=='?'){
 
-    }
+                s[3]='5';
+            }
+
+            if( s[4]=='?'){
+
+                s[4]='9';
+            }
+            
         
         return s;
     }
