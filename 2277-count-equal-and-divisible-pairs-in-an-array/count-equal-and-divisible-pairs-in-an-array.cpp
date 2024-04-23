454ecac743e4eca236900de8e1d9d31c4f2669e9
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-04-23 16:53:26 +0530

    Time: 4 ms (65.87%) | Memory: 14.5 MB (18.01%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -4,10 +4,8 @@ public:
         int count=0;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    if((i*j)%k==0){
-                        count++;
-                    }
+                if(nums[i]==nums[j] && (i*j)%k==0){
+                    count++;
                 }
             }
         }
