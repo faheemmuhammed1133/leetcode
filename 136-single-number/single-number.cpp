df7e4ac534f06e668fec045e14c5c0cfb558a667
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-05-02 17:24:54 +0530

    Time: 20 ms (25.14%) | Memory: 20.3 MB (26.57%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& n) {
-        if(n.size()==1){
-            return n[0];
-        }
+        // if(n.size()==1){
+        //     return n[0];
+        // }
         sort(n.begin(),n.end());
         int res= n[0];
         for(int i=1;i<n.size();i++){
