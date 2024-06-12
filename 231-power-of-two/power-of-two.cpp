8c26cd75e0abe4e2b072ba719763357b8a3e9686
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-06-12 12:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (36.39%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,9 +5,6 @@ public:
             return true;
         }
         for(long int i=2;i<=n;i*=2){
-            // if(i*2>=INT_MAX){
-            //     return false;
-            // }
             if(i==n){
                 return true;
             }
