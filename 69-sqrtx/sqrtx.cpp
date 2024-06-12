fc53ab9192798f96e5801ea764ca4d35ccc24d97
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-06-12 12:18:31 +0530

    Time: 17 ms (10.04%) | Memory: 7.4 MB (39.33%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,18 +1,1 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        for(int i=0;i<=x/2+1;i++){
-            if((i)*(i)>=x ){ 
-                if((i)*(i)!=x){
-                    return i-1;
-                }
-            
-                return i;
-            }
-            if(i==46340){
-                return i;
-            }   
-        }
-        return 1;
-    }
-};
+class Solution {public:int mySqrt(int x){for(int i=0;i<=x/2+1;i++){if((i)*(i)>=x){if((i)*(i)!=x){return i-1;}return i;}if(i==46340){return i;}}return 1;}};
