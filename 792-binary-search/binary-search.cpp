2c78e186bb3e1089e80ca95ddfbae222518e0394
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-10-18 11:56:58 +0530

    Time: 2 ms (99.90%) | Memory: 30.3 MB (21.75%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int t) {
+        int r=0;
+        int l=nums.size()-1;
+        while(r<=l){
+            int mid=(r+l)/2;
+
+            if(nums[mid]==t){
+                return mid;
+            }
+            if(nums[mid]>t){
+                l=mid-1;
+            }
+            if(nums[mid]<t){
+                r=mid+1;
+            }
+            cout<<nums[mid]<<" "<<t<<"\n";
+        }
+        return -1; 
+    }
+};
