f6f1cd115f594eaf46c5b22c10ae9e2f22d58cdf
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-01-06 14:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.26%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int p1=0,p2 =0; 
+        while(p1<s.length() && p2 <t.length()){
+            if(s[p1]==t[p2]){
+                p1++;
+                cout<<p1<<" \n";
+            }
+            p2++;
+        }
+        return p1==s.length();
+    }
+};
