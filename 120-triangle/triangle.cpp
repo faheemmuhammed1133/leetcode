49703c44dec85ed96c3f38c1cabf66f25a6f14df
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-01-07 11:11:56 +0530

    Time: 3 ms (14.09%) | Memory: 12.8 MB (49.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) {
-        int l=t.size()-1;
-        vector<int> sum(t[l]);
-        for(int i=l-1;i>=0;i--){
+        vector<int> sum(t[t.size()-1]);
+        for(int i=t.size()-2;i>=0;i--){
             for(int j=0;j<=i;j++){
                 sum[j]=t[i][j]+min(sum[j],sum[j+1]);
             }
