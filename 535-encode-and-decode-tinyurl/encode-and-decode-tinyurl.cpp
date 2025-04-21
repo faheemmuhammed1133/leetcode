7925d6cede788737ad1b356eecd90fd7f15b599d
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-04-21 08:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (76.41%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    int i = 0 ;
-    unordered_map<int, std::string> map;
+
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        string str = "http://tinyurl.com/";
-        map.insert({i,longUrl});
-        str = str + to_string(i);
-        cout<<str<<" ";
-        return str ;
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        string str = "http://tinyurl.com/";
-        string sub = shortUrl.substr(str.length());
-        int index = stoi(sub);
-        string answer = map[index];
-        return answer;
+        return shortUrl;
     }
 };
-
-// Your Solution object will be instantiated and called as such:
-// Solution solution;
-// solution.decode(solution.encode(url));
