2907a064636822f840bbff3880d2d086bb6ca6d0
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-05-08 23:21:45 +0530

    Time: 8 ms (6.76%) | Memory: 25 MB (72.13%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        vector<int> freq(101,0);
+        for (int i=0;i<nums.size()-1;i++){
+            for (int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    freq[i]+=1;
+                    cout<<freq[i]<<" -> i "<<i<< " j -> "<<j<<" \n";
+                }
+                if(freq[i]>1) return false;
+            }
+        }
+        
+        return true;
+    }
+};
