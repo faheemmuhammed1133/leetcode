bfdfc7df5f11345ba33f93a070a6be79d145d401
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-05-08 23:24:20 +0530

    Time: 7 ms (6.76%) | Memory: 25.2 MB (56.42%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        vector<int> freq(101,0);
-        for (int i=0;i<nums.size()-1;i++){
-            for (int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    freq[i]+=1;
-                }
-                if(freq[i]>1) return false;
+        unordered_map<int,int> freq;
+         for (int x : nums) {
+            if (++freq[x] > 2) {
+                return false;
             }
         }
-        
         return true;
     }
 };
