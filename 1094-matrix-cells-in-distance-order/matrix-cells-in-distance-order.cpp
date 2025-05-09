1d26a219ae833a90f19362074568f3bae85efa9f
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-05-09 23:31:35 +0530

    Time: 4 ms (98.29%) | Memory: 19.6 MB (85.61%) - LeetSync

---
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        // generating cells
+        vector<vector<int>> cells;
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                cells.push_back({r, c});
+            }
+        }
+        
+        // 2. Sort by Manhattan distance from (rCenter, cCenter)
+        sort(cells.begin(), cells.end(),
+             [rCenter, cCenter](const vector<int>& a, const vector<int>& b) {
+                 int da = abs(a[0] - rCenter) + abs(a[1] - cCenter);
+                 int db = abs(b[0] - rCenter) + abs(b[1] - cCenter);
+                 return da < db;
+             });
+        
+        return cells;
+    }
+};
