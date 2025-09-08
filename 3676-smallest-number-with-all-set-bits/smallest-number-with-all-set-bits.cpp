fbf00a22a9bbf795f1db653566a5b84abe802f64
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-09-08 16:15:22 +0530

    Time: 6 ms (1.62%) | Memory: 8.2 MB (62.81%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        if ( n <=1 ){
+            return n;
+        }
+        int pow2 = pow(2 ,0);
+        int i =1 ;
+        int n2 = n*n;
+        while (i<n2){
+            if(pow2 > n){
+                return pow2-1;
+            }
+            cout<< pow2;
+            pow2 =  pow(2 ,i);
+            i++;
+        }
+
+        return pow2-1;
+    }
+};
