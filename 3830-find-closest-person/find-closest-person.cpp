48a0ba36972450ad21be0fae539dfb5cfa9d0552
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-09-28 22:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (88.39%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -3,8 +3,7 @@ public:
     int findClosest(int x, int y, int z) {
         int zx = abs(x-z);
         int zy = abs(y-z);
-        if (x==y | zx == zy) return 0;
+        if (x==y || zx == zy) return 0;
         return zx < zy ? 1 : 2;
-
     }
 };
