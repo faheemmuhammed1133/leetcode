4017127af3e261c44050789f97bf1931d5dd1a8c
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2026-09-02 09:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (76.44%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int digitSum(int n){
+        int sum=0;
+        while( n >0) {
+            sum+=n%10;
+            n=n/10;
+            }
+        return sum;
+    }
+    int digitFrequencyScore(int n) {
+        return digitSum(n);
+    }
+};
+
