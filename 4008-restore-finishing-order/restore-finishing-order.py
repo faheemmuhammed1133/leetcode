490d57e4ae429494d370bbd4c6c947b82886133e
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2026-09-02 09:13:03 +0530

    Time: 5 ms (6.62%) | Memory: 19.3 MB (19.02%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.py
+++ 4008-restore-finishing-order/restore-finishing-order.py
@@ -1,22 +1,14 @@
 class Solution:
 
-    def binary_search_iterative(self,arr, target):
-        low = 0
-        high = len(arr) - 1
-
-        while low <= high:
-            mid = low + (high - low) // 2
-            if arr[mid] == target:
-                return arr[mid]
-            elif arr[mid] < target:
-                low = mid + 1
-            else:
-                high = mid - 1
+    def binary_search_bisect(self,arr, target):
+        i = bisect.bisect_left(arr, target)
+        if i < len(arr) and arr[i] == target:
+            return arr[i]
         return -1
 
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
         res = []
         for i in order :
-            if self.binary_search_iterative(friends,i) != -1:
-                res.append(self.binary_search_iterative(friends,i))
+            if self.binary_search_bisect(friends,i) != -1:
+                res.append(self.binary_search_bisect(friends,i))
         return res
