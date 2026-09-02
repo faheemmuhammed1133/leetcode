e1ea525abb8b048580da66ae213191bb5599b3bd
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2026-09-02 09:09:07 +0530

    Time: 7 ms (6.62%) | Memory: 19.3 MB (19.02%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.py
+++ 4008-restore-finishing-order/restore-finishing-order.py
@@ -1,8 +1,22 @@
 class Solution:
+
+    def binary_search_iterative(self,arr, target):
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if arr[mid] == target:
+                return arr[mid]
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
         res = []
         for i in order :
-            for j in friends:
-                if i ==j:
-                    res.append(i)
+            if self.binary_search_iterative(friends,i) != -1:
+                res.append(self.binary_search_iterative(friends,i))
         return res
