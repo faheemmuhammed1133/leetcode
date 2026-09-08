d17c55e8402688a5f7588298eead6de6e2534abc
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2026-09-08 12:50:03 +0530

    Time: 18 ms (7.29%) | Memory: 19.4 MB (34.27%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,35 +1,12 @@
 class Solution:
-
-    # indexing to find vahracter
-    def indexCharacter(self, words):
-        wordsIndexes = [[ord(ch.lower())-97 for ch in word] for word in words]
-        print(wordsIndexes)
-
-        return wordsIndexes
-
-    def replaceWordValue(self , wordsIndexes,weights):
-        wordsIndexValue = []
-        for row in wordsIndexes:
-            arr = []
-            for col in row:
-                col = weights[col]
-                arr.append(col)
-            wordsIndexValue.append(arr)
-
-
-        print(wordsIndexValue)
-        return wordsIndexValue
-
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        wordsIndexes = self.indexCharacter( words)
-        wordsIndexValue = self.replaceWordValue(wordsIndexes,weights)
-
+        # index of each char
+        wordsIndexes = [[ord(ch.lower())-97 for ch in word] for word in words]
+        # representative value of the char
+        wordsIndexValue = [[weights[val] for val in words]for words in wordsIndexes]
+        # sum of each row 
         word_sum = [sum(val for val in row) for row in wordsIndexValue]
-        print(word_sum)
-
         # Modulo + final char 
         final_index = [25-(val%26) for val in word_sum]
-        ans = ""
-        for val in final_index:
-            ans+=chr(val+97)
+        ans = "".join(chr(val + 97) for val in final_index)
         return ans
