0b3ad76f7e91d2a5394f1082a383527ac11c3848
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2026-09-08 13:24:35 +0530

    Time: 13 ms (23.25%) | Memory: 19.3 MB (34.27%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,12 +1,17 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        # index of each char
+        """# index of each char
         wordsIndexes = [[ord(ch.lower())-97 for ch in word] for word in words]
         # representative value of the char
         wordsIndexValue = [[weights[val] for val in words]for words in wordsIndexes]
         # sum of each row 
         word_sum = [sum(val for val in row) for row in wordsIndexValue]
         # Modulo + final char 
-        final_index = [25-(val%26) for val in word_sum]
+        final_index = [25-(val%26) for val in word_sum]"""
+        
+        final_index = [25-(sum(weights[ord(ch.lower())-97] for ch in word)%26) for word in words]
+        print(final_index)
+        ans= ""
+
         ans = "".join(chr(val + 97) for val in final_index)
         return ans
